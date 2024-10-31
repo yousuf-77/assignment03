e20ca3b8f7bdb6285043a490d347b858af79430e
Muhammad Yousuf <148687228+yousuf-77@users.noreply.github.com>
2024-11-01 03:00:36 +0500

    resolved deployment issue
    resolved deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ const HomePage=()=>{
   return(
     <main>
     <div className="bg-[url(/images/purple-bgc.jpg)] bg-no-repeat bg-cover h-[75vh]">
-    <div className="flex pt-[80px] items-center justify-center">
-    <div className="w-[60%] px-[40px]">
+    <div className="flex pt-[80px] items-center justify-around">
+    <div className="w-[50%] px-[40px] ml-9">
       <h1 className="text-5xl font-bold mb-[16px] text-yellow-100">Lorem ipsum dolor sit </h1>
       <h3 className="text-xl font-medium mb-[12px] w-[90%] text-yellow-100" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quam vitae veritatis odio consectetur Magnam quis officiis voluptates quia?</h3>
     </div>
-    <div className="w-[40%]"><Image className=" rounded-[16px]" src="/images/man-computer.jpg" alt="" width={800} height={600}/></div>
+    <div className="w-[50%]"><Image className=" rounded-[16px]" src="/images/man-computer.jpg" alt="" width={600} height={400}/></div>
     </div>
     </div>
     <AboutUs></AboutUs>
--- components/portfolio-component/portfolio.tsx
+++ components/portfolio-component/portfolio.tsx
@@ -5,7 +5,7 @@ return (
 <div className="text-white text-[52px] font-bold text-center backdrop-blur-sm ">Portfolio</div>
       <div className="flex items-center justify-around backdrop-blur-sm h-[450px] pt-[20px]">
         <div className="w-[60%] p-[40px] ml-16">
-          <h1 className="text-white text-5xl font-bold mb-[16px]">I'm Muhammad Yousuf</h1>
+          <h1 className="text-white text-5xl font-bold mb-[16px]">I am Muhammad Yousuf</h1>
           <h2 className="text-white text-4xl font-semibold mb-[16px]">Front End Developer</h2>
           <h3 className="text-white text-xl font-medium mb-[12px] w-[95%]">I am currently expanding my skill set in full-stack development and artificial intelligence at GIAIC. Passionate and excited about these fields, I look forward to applying my knowledge to create innovative solutions and drive impactful projects.</h3>
         </div>
